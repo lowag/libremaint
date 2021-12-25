3293a037c97a27af547bbeca1a2620a99ef194fc
lowag <a.veres74@gmail.com>
2021-12-25 18:14:05 +0100

    upgrade chart.js

--- includes/lm-header.php
+++ includes/lm-header.php
@@ -50,8 +50,8 @@ echo "<script src=\"".VENDORS_LOC."jquery-ui/jquery-ui.min.js\"></script>\n";
     echo "<script src=\"".INCLUDES_LOC."for_fancytree.js\"></script>\n";
 
     }
-    
-    echo "<script src=\"".VENDORS_LOC."chart.js/dist/Chart.min.js\"></script>\n";
+    echo "<script src=\"".VENDORS_LOC."moment.js\"></script>\n";
+    echo "<script src=\"".VENDORS_LOC."chart.js/package/dist/chart.min.js\"></script>\n";
 
     
     echo "<script src=\"".VENDORS_LOC."popper.js/dist/umd/popper.min.js\"></script>\n";
